67aab313cc3fefbbaedcfac02452da05351c0de9
Marketgai <marcusalee@gmail.com>
2020-10-07 16:02:07 -0700

    finished functional pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,7 @@
 var Queue = function() {
   var someInstance = {};
+  var queueBack = 0;
+  var queueFront = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +9,25 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    queueBack++;
+    storage[queueBack] = value;
   };
 
   someInstance.dequeue = function() {
+    if (queueFront <= queueBack) {
+      queueFront++;
+      let dequeued = storage[queueFront];
+      delete storage[queueFront];
+      return dequeued;
+    }
   };
 
   someInstance.size = function() {
+    if (queueBack - queueFront < 0) {
+      return 0;
+    } else {
+      return (queueBack - queueFront);
+    }
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,27 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var stackSize = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    //create a key value pair numerically
+    stackSize++;
+    storage[stackSize] = value;
   };
 
   someInstance.pop = function() {
+    let popped = storage[stackSize];
+    delete storage[stackSize];
+    if (stackSize > 0) {
+      stackSize--;
+    }
+    return popped;
   };
 
   someInstance.size = function() {
+    return stackSize;
   };
 
   return someInstance;
 };
+
