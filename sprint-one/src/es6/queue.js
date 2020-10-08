54336489127602a5dffd3fdd90a77cd30283e99f
Marketgai <marcusalee@gmail.com>
2020-10-08 04:18:44 -0700

    almost done with ES6 queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,30 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.head = 0;
+    this.tail = 0;
   }
 
+  enqueue(value) {
+    this.tail++;
+    this.storage[this.tail] = value;
+  }
+
+  dequeue() {
+    this.head++;
+    if (this.tail - this.head > 0) {
+      let removedVal = this.storage[this.head];
+      delete this.storage[this.head];
+      return removedVal;
+    }
+  }
+
+  size() {
+    if (this.tail - this.head <= 0) {
+      return 0;
+    } else {
+      return this.tail - this.head;
+    }
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,26 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
   constructor() {
+    this.storage = {};
+    this.stackSize = 0;
+  }
+
+  push(value) {
+    this.stackSize++;
+    this.storage[this.stackSize] = value;
+  }
+
+  pop() {
+    if (this.stackSize > 0) {
+      let removedVal = this.storage[this.stackSize];
+      delete this.storage[this.stackSize];
+      this.stackSize--;
+      return removedVal;
+    }
+  }
+
+  size() {
+    return this.stackSize;
   }
 
 }
